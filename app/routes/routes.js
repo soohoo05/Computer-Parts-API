c57796de6f5dd288b1638baf17e536447eb29610
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2019-02-28 15:45:44 -0500

    basics

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,54 @@
+var ObjectID= require('mongodb').ObjectID
 module.exports = function(app,db){
-
   app.post('/jobs',(req,res)=>{
-    res.send()
+    const job={Company:req.body.Company,Date:req.body.Date}
+    db.collection('jobs').insert(job,(err,res)=>{
+      if(err){
+        res.send({'error':'An error has occured'})
+      }
+      else{
+        res.send(result.ops[0])
+      }
+    })
   })
 
+  app.put('/jobs/:id',(req,res)=>{
+    const id= req.params.id
+    const details={'_id':new ObjectID(id)}
+    const job={Company:req.body.Company,Date:req.body.Date}
+    db.collection('jobs').update(details,(err,item)=>{
+      if(err){
+        res.send({'error':'An error has occured'})
+      }
+      else{
+        res.send(item)
+      }
+    })
+  })
+
+  app.get('/jobs/:id',(req,res)=>{
+    const id= req.params.id
+    const details={'_id':new ObjectID(id)}
+    db.collection('jobs').findOne(details,(err,item)=>{
+      if(err){
+        res.send({'error':'An error has occured'})
+      }
+      else{
+        res.send(item)
+      }
+    })
+  })
+
+  app.delete('/jobs/:id',(req,res)=>{
+    const id= req.params.id
+    const details={'_id':new ObjectID(id)}
+    db.collection('jobs').remove(details,(err,item)=>{
+      if(err){
+        res.send({'error':'An error has occured'})
+      }
+      else{
+        res.send('Deleted')
+      }
+    })
+  })
 }
--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require ('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express()
-
+const db = require('./config/db')
 const port = 3000;
 
-require('./app/routes')(app,{})
 
-app.user(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.listen(port, ()=>{
-  console.log("live on "+port)
-})
+
+
+ MongoClient.connect(db.url,{useNewUrlParser:true})
+
+
+
+
+
+ require('./app/routes')(app,db)
+ app.listen(port, ()=>{
+   console.log("live on "+port)
+ })
